f5b63973a781aab3784b9402cd2e48fd77b3a5e4
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-06-01 17:48:28 -0500

    Finished/fixed Buildings part of exercise

--- Building.cs
+++ Building.cs
@@ -4,49 +4,49 @@ using System.Collections.Generic;
 namespace Planner {
 
     class Building {
-        public Planner()
-        {
-            Designer = "";
-            dateConstructed = DateTime.Now;
-            Address = "";
-            Owner = "";
+
+        private string _designer = "Meag Mueller";
+
+        private DateTime _dateConstructed { get; set; }
+
+        private string _address { get; set; }
+
+        private string _owner { get; set; }
+
+        public int Stories { get; set; }
+
+        public double Width { get; set; }
+
+        public double Depth { get; set; }
+
+        public double Volume {
+            get 
+                {
+                    return (Width*Depth(3*Stories));
+                }
         }
 
-        private string Designer() { get; set; }
-
-        private DateTime dateConstructed() { get; set; }
-
-        private string Address() { get; set; }
-
-        private string Owner() { get; set; }
-
-        public int Stories() { get; set; }
-
-        public double Width() { get; set; }
-
-        public double Depth() { get; set; }
-
-        // public readonly double Volume() {
-        //     get {
-        //         return (Width*Depth(3*Stories));
-        //     }
-        // }
+        public string Designer() {
+            return _designer;
+        }
 
         public void AddressConstructor(string address)
         {
-            Console.WriteLine("This is the thing right now.");
             // The constructor's logic should set the _address field's value to the argument value
+            _address = address;
         }
 
         public void Construct()
         {
             // Define a Construct() method. The method's logic should set the _dateConstructed field's value to DateTime.Now.
-            DateTime dateConstructed = DateTime.Now;
+            _dateConstructed = DateTime.Now;
         }
 
-        public void Purchase()
+        public void Purchase(string NewOwner)
         {
             // Define a Purchase() method. The method should accept a single string argument of the name of the person purchasing a building. The method should take that string and assign it to the private _owner field.
+
+            _owner = NewOwner;
 
         }
 
