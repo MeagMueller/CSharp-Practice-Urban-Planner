a0acb9f9135ecb25aa2a986cc5bd40009ce187da
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-06-01 19:20:27 -0500

    Working

--- Program.cs
+++ Program.cs
@@ -1,15 +1,62 @@
 using System;
 using System.Collections.Generic;
 
-class Program {
+namespace Planner
+{
+
+    class Program
+    {
 
         static void Main(string[] args)
         {
-            Building building1 = new Building("500 Interstate Blvd.") {
+
+            // Creating buildings
+            Building building1 = new Building("500 Interstate Blvd.")
+            {
                 Width = 25.5,
                 Depth = 30.6,
                 Stories = 100,
-                
+
             };
+
+            Building building2 = new Building("500 SomeOther Blvd.")
+            {
+                Width = 486.5,
+                Depth = 5482,
+                Stories = 5,
+            };
+
+            Building building3 = new Building ("500 Another Place")
+            {
+                Width = 2545,
+                Depth = 324,
+                Stories = 22,
+            };
+
+            // Constructing buildings using method
+            building1.Construct();
+            building2.Construct();
+            building3.Construct();
+
+            // Purchasing buildings using method
+            building1.Purchase("Simon");
+            building2.Purchase("Betsy");
+            building3.Purchase("Kiri");
+
+            // Creating a list to iterate over
+
+            List<Building> BuildingsList = new List<Building>() {
+                building1, 
+                building2,
+                building3
+            };
+
+            foreach(Building building in BuildingsList) {
+                Console.WriteLine(building.AddressConstructor);
+                Console.WriteLine($"Designed by {building.Designer}");
+                Console.WriteLine("-------------");
+                Console.WriteLine();
+            }
         }
     }
+}
